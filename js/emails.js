2a94d8c5cb0e5a4649767c40e16884060badea7a
Hiram Abraham <hiramabraham@Hirams-MacBook-Pro.local>
2019-07-01 19:43:43 -0500

    Changed file hierarchy to prevent repo clashing

---
+++ js/emails.js
@@ -0,0 +1,12 @@
+// const sgMail = require('@sendgrid/mail');
+
+// const sendgridAPIKey = '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';
+
+// sgMail.setApiKey(sendgridAPIKey);
+
+// sgMail.send({
+//     to: 'hgabraham.dev@gmail.com',
+//     from: 'hgamab12@gmail.com',
+//     subject: 'How ya derrrin',
+//     text: 'Second try. The last one was kinda slow mah bois.'
+// })
